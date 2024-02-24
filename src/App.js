9bb18002187f8f7ca548839a0235514e2aca4277
mrafeeqvr <muhammedrafeeq805@gmail.com>
2024-02-24 13:36:26 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
+import Categories from './components/Categories';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path : '/',
+      element : <Home/>
+    },
+    {
+      path : '/categories',
+      element : <Categories/>
+    },
+  ])
   return (
-    <div>
-      <Home/>
-    </div>
+    <RouterProvider router={router}>
+    </RouterProvider>
+
   );
 }
 
