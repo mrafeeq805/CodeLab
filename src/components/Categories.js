ef95949cba8b2d4859cf1a469c1f93da29ff6357
mrafeeqvr <muhammedrafeeq805@gmail.com>
2024-02-24 13:54:10 +0530

    categories page designed

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
+import SearchBar from './SearchBar'
+import CategoriesSection from './CategoriesSection'
 
 const Categories = () => {
   return (
     <div>
         <Navbar title={"Categories"}/>
+        <SearchBar title={"Search Categories..."}/>
+        <CategoriesSection/>
     </div>
   )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchBar = ({title}) => {
 	return (
-		<div className="p-2">
+		<div className="p-2 my-2">
 			<div className="rounded-lg border-2 p-2 px-4 flex justify-between">
 				<input type="text" placeholder={title} />
                 <i class="bi bi-search text-gray-500"></i>
