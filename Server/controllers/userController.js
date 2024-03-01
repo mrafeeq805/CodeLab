bec903a7c7e1f37eef02a30ff0fa96d56a6a54ad
mrafeeqvr <muhammedrafeeq805@gmail.com>
2024-03-01 20:37:00 +0530

    password secured on create account

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,17 +1,29 @@
 const userSchema = require("../models/user");
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 
 
 module.exports = {
 	createaccount: async (req, res) => {
         const {name,email,password} = req.body
-        const newuser = new userSchema({
-            name : name,
-            email : email,
-            password : password
-        })
-        newuser.save()
-        res.json()
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+            const newuser = new userSchema({
+                name : name,
+                email : email,
+                password : hash,
+                avatar : '',
+                projects : [],
+                bio : '', 
+                title : ''
+            })
+            newuser.save()
+            res.json()
+        });
+        
+        
 		
 	},
     
